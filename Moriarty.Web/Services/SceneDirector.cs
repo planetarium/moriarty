016c757c19b5311212bc8ef431128768188c30d6
Swen Mun <longfinfunnel@gmail.com>
2024-08-26 15:58:17 +0900

    Fix scene director

--- Moriarty.Web/Services/SceneDirector.cs
+++ Moriarty.Web/Services/SceneDirector.cs
@@ -9,21 +9,27 @@ public class SceneDirector(Kernel kernel, PromptLoader promptLoader, ILoggerFact
 {
     private readonly ILogger<SceneDirector> _logger = loggerFactory.CreateLogger<SceneDirector>();
 
-    public async Task InvokeAsync(Guid campaignId, ChatHistory chatHistory, CancellationToken cancellationToken)
+    public async Task InvokeAsync(
+        Guid campaignId,
+        ChatHistory chatHistory,
+        string currentScene,
+        CancellationToken cancellationToken)
     {
         string systemPrompt = promptLoader.Load("SceneDirector.txt");
-        systemPrompt += $"\n Current Campaign is {campaignId}";
+        string functionPrompt = promptLoader.Load("DirectScene.txt");
         KernelFunction function = kernel.CreateFunctionFromPrompt(
-            "Describe the place and display the character card where the conversation takes place. Chat History: {{$chatHistory}}",
+            functionPrompt,
             executionSettings: new OpenAIPromptExecutionSettings()
             {
                 ModelId = "gpt-4o",
                 ChatSystemPrompt = systemPrompt,
                 ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
             });
-        var res = await kernel.InvokeAsync(function, new()
+        _ = await kernel.InvokeAsync(function, new()
         {
-            { "chatHistory", string.Join("\n", chatHistory) },
+            { "chat_history", string.Join("\n", chatHistory) },
+            { "campaign_id", campaignId.ToString() },
+            { "current_scene", currentScene },
         }, cancellationToken);
     }
 }
